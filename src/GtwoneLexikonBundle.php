6c239d0393d696ac19468877138d0cdc326c00a4
gtwone <31689473+gtwone@users.noreply.github.com>
2017-09-06 16:42:21 +0200

    Add files via upload

---
+++ src/GtwoneLexikonBundle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright  Glen Langer 2008..2017 <http://contao.ninja>
+ * @author     Glen Langer (BugBuster)
+ * @package    Xing
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/BugBuster1701/contao-xing-bundle
+ *
+ */
+namespace Gtwone\LexikonBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+/**
+ * Configures the Contao xing bundle.
+ *
+ * @author Glen Langer
+ */
+class GtwoneLexikonBundle extends Bundle
+{
+}
